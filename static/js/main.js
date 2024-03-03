fe9887fd474e6ba371d805e83cdb1043c478585d
Serdar Tumgoren <zstumgoren@gmail.com>
2024-03-03 14:35:38 -0800

    Remove cors and apikey versions and move open/index.html to top-level dir

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 function renderSearchResults(data) {
-    console.log(data);
+    //console.log(data);
     const resultsDiv = document.querySelector('#search-results');
     const area = data.area;
     const vlSpec = {
@@ -8,29 +8,33 @@ function renderSearchResults(data) {
       "data": {"values": data.data},
       "mark": "line",
       "encoding": {
-        "x": {"field": "date", "type": "temporal"},
-        "y": {"field": "unemployed_rate", "type": "quantitative"}
-      }
-    }
+        "x": {"field": "date", "type": "temporal", "timeUnit": "yearmonth"},
+        "y": {"field": "unemployed", "type": "quantitative"}      }
+    };
     vegaEmbed('#vis', vlSpec);
   };
 
 // Use currying to configure onsubmit callback function that targets 
 // a particular API endpoint 
-function submitSearch(url) {
+function submitSearch(url, apiKey) {
     return (event) => {
     // Prevent normal form action of changing page URL based on form values;
-    event.preventDefault(); 
+    event.preventDefault();
     const county = document.querySelector('#county').value;
     const state = document.querySelector('#state').value;
     console.log(`${county}, ${state}`);
-    const query_url = `${url}?county=${county}&state=${state}`;
-    console.log(query_url);
+    console.log(`API Key: ${apiKey}`)
+    let query_url = new URL(url);
+    query_url.searchParams.set('county', county);
+    query_url.searchParams.set('state', state);
+    if (typeof apiKey !== 'undefined') {
+      query_url.searchParams.set('key', apiKey);
+    }
+    console.log(query_url.toString());
     const data = fetch(query_url)
       .then(response => response.json())
       .then(data => {
         renderSearchResults(data);
-        //console.log(data)
       });
     };
   };
