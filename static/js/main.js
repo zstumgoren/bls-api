0c66d4a049025795db9f9483b946e81fd67e89be
Serdar Tumgoren <zstumgoren@gmail.com>
2024-03-04 12:30:59 -0800

    Reorg main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,28 @@
+// Use currying to configure onsubmit callback function that 
+// accepts url and optional API key
+function submitSearch(url, apiKey) {
+  return (event) => {
+  // Prevent normal form action of changing page URL based on form values;
+  event.preventDefault();
+  const county = document.querySelector('#county').value;
+  const state = document.querySelector('#state').value;
+  console.log(`${county}, ${state}`);
+  console.log(`API Key: ${apiKey}`)
+  let query_url = new URL(url);
+  query_url.searchParams.set('county', county);
+  query_url.searchParams.set('state', state);
+  if (typeof apiKey !== 'undefined') {
+    query_url.searchParams.set('key', apiKey);
+  }
+  console.log(query_url.toString());
+  const data = fetch(query_url)
+    .then(response => response.json())
+    .then(data => {
+      renderSearchResults(data);
+    });
+  };
+};
+
 function renderSearchResults(data) {
 
     const resultsDiv = document.querySelector('#search-results');
@@ -13,29 +38,4 @@ function renderSearchResults(data) {
       }
     };
     vegaEmbed('#vis', vlSpec);
-  };
-
-// Use currying to configure onsubmit callback function that targets 
-// a particular API endpoint 
-function submitSearch(url, apiKey) {
-    return (event) => {
-    // Prevent normal form action of changing page URL based on form values;
-    event.preventDefault();
-    const county = document.querySelector('#county').value;
-    const state = document.querySelector('#state').value;
-    console.log(`${county}, ${state}`);
-    console.log(`API Key: ${apiKey}`)
-    let query_url = new URL(url);
-    query_url.searchParams.set('county', county);
-    query_url.searchParams.set('state', state);
-    if (typeof apiKey !== 'undefined') {
-      query_url.searchParams.set('key', apiKey);
-    }
-    console.log(query_url.toString());
-    const data = fetch(query_url)
-      .then(response => response.json())
-      .then(data => {
-        renderSearchResults(data);
-      });
-    };
   };
